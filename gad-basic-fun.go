7583f8e977903d90233b13c0b5661454250599ce
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2023-08-21 22:12:11 +0200

    Updating gad-basic-fun.go

--- gad-basic-fun.go
+++ gad-basic-fun.go
@@ -20,11 +20,23 @@ func calculateBill(price int, productNo int) int {
 	return totalPrice
 }
 
-// Multiple return values:
+// Multiple return values (1):
 func rectangleProps(length, width float64) (float64, float64) {
 	var area = length * width
 	var perimeter = (length * width) * 2
 	return area, perimeter
+}
+
+// Multiple return values (2):
+func sumAndSub(num1 int, num2 int) (int, int) {
+	mySum := num1 + num2
+	var mySub = num1 - num2
+	return mySum, mySub
+}
+
+// Lets return multiple values of different data types
+func getDefaultValues() (int, string) {
+	return 0, "Not Known"
 }
 
 // Names return values
@@ -37,22 +49,34 @@ func rectanglePropsNamed(length, width float64) (area, perimeter float64) {
 
 // Using Blank identifier (_)
 // Used if we only need the area and want to discard the parameter
-
 func getRectangleProps(length, width float64) (float64, float64) {
 	var area = length * width
 	var perimeter = (length + width) * 2
 	return area, perimeter
 }
 
+func sayHello() {
+	fmt.Println("Good Morning!")
+}
+
 func main() {
 	fmt.Println("********************************************************")
-
+	sayHello() // Greeting function
 	// Calling named function
 	fmt.Println(rectanglePropsNamed(10, 4))
 
 	// Calling a function with Blank identifier
 	myArea, _ := getRectangleProps(20.4, 3.2)
-	fmt.Printf("The area is: %f", myArea)
+	fmt.Printf("The area is: %f \n", myArea)
+
+	// Calling sumAndSub function
+	var mySum, mySub = sumAndSub(50, 5)
+	fmt.Println("The sum is: ", mySum, "The sub is: ", mySub)
+
+	// Calling n using function with the default value!
+	myId, firstname := getDefaultValues()
+	fmt.Println("ID: ", myId, ", First Name: ", firstname)
+	fmt.Println("************************************************")
 
 	// 1st Usage:
 	addNumbers(34, 66)
