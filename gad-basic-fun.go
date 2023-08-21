4cbd72bb59631172c7f1e0f18317df0a4d850eb0
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2023-08-21 21:50:35 +0200

    Last update gad-basic-fun.go

--- gad-basic-fun.go
+++ gad-basic-fun.go
@@ -27,8 +27,32 @@ func rectangleProps(length, width float64) (float64, float64) {
 	return area, perimeter
 }
 
+// Names return values
+func rectanglePropsNamed(length, width float64) (area, perimeter float64) {
+	area = length * width
+	perimeter = (length + width) * 2
+	// No explicit return value...
+	return
+}
+
+// Using Blank identifier (_)
+// Used if we only need the area and want to discard the parameter
+
+func getRectangleProps(length, width float64) (float64, float64) {
+	var area = length * width
+	var perimeter = (length + width) * 2
+	return area, perimeter
+}
+
 func main() {
 	fmt.Println("********************************************************")
+
+	// Calling named function
+	fmt.Println(rectanglePropsNamed(10, 4))
+
+	// Calling a function with Blank identifier
+	myArea, _ := getRectangleProps(20.4, 3.2)
+	fmt.Printf("The area is: %f", myArea)
 
 	// 1st Usage:
 	addNumbers(34, 66)
